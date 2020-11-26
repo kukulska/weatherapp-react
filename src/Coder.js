f5b3b0f66cb46cc968819f438999981ceb564b92
Agata Kukulska <a.kukulska@pm.me>
2020-11-26 15:26:32 +0000

    Corrected github link

--- src/Coder.js
+++ src/Coder.js
@@ -5,7 +5,7 @@ export default function Coder() {
   return (
     <span className="Coder">
       <a
-        href="https://github.com/kukulska/weather-app"
+        href="https://github.com/kukulska/weatherapp-react"
         target="_blank"
         rel="noreferrer"
       >
