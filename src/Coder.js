50d018f27e07b3b825ea5e41086a88930a510781
Agata Kukulska <a.kukulska@pm.me>
2020-11-26 12:34:15 +0000

    Added rel = "noreferrer"

--- src/Coder.js
+++ src/Coder.js
@@ -4,7 +4,11 @@ import "./Coder.css";
 export default function Coder() {
   return (
     <span className="Coder">
-      <a href="https://github.com/kukulska/weather-app" target="_blank">
+      <a
+        href="https://github.com/kukulska/weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-source code
       </a>
       <span> by Agata Kukulska</span>
